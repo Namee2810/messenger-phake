a6e2435090b5f7d4729908bf88dd52d53c52694b
Namee2810 <62344286+Namee2810@users.noreply.github.com>
2021-02-04 21:34:04 +0700

    Update axiosClient.js

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import queryString from "query-string";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
     'X-Requested-With': 'XMLHttpRequest',
